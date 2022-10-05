9764d6c51d49ded2e6f56ac59e54b9526849cf08
Atirsaw <awokeeyrw@gmail.com>
2022-10-05 13:51:43 +0300

    Intial Commit for the jokes app

---
+++ src/test/java/ec/springframework/springjokesapp/SpringJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.springframework.springjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
